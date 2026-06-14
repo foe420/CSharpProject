fb07f124c649a197942ad18d5990d80522939055
def <truykich1st@gmail.com>
2026-06-14 16:15:15 +0700

    update db migration

--- backend/src/TuneVault.Infrastructure/Migrations/20260612102132_AddApplicationUserTable.Designer.cs
+++ backend/src/TuneVault.Infrastructure/Migrations/20260612102132_AddApplicationUserTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TuneVault.Infrastructure.Persistence;
 
@@ -11,9 +12,11 @@ using TuneVault.Infrastructure.Persistence;
 namespace TuneVault.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260612102132_AddApplicationUserTable")]
+    partial class AddApplicationUserTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
